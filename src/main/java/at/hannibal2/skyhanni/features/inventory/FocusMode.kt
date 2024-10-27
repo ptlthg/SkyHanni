50ee0e0c4e8daad88a225a84b675277229e516c6
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2024-10-27 23:28:03 +0100

    Improvement: Focus Mode Options (#2844)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/inventory/FocusMode.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/FocusMode.kt
@@ -5,6 +5,7 @@ import at.hannibal2.skyhanni.events.LorenzTickEvent
 import at.hannibal2.skyhanni.events.LorenzToolTipEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
 import at.hannibal2.skyhanni.utils.InventoryUtils
+import at.hannibal2.skyhanni.utils.KeyboardManager
 import at.hannibal2.skyhanni.utils.KeyboardManager.isKeyClicked
 import at.hannibal2.skyhanni.utils.LorenzUtils
 import net.minecraftforge.fml.common.eventhandler.EventPriority
@@ -15,20 +16,36 @@ object FocusMode {
 
     private val config get() = SkyHanniMod.feature.inventory.focusMode
 
-    private var toggle = true
+    private var active = false
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onLorenzToolTip(event: LorenzToolTipEvent) {
-        if (!isEnabled() || !toggle) return
+        if (!isEnabled()) return
         if (event.toolTip.isEmpty()) return
-        event.toolTip = mutableListOf(event.toolTip.first())
+        val keyName = KeyboardManager.getKeyName(config.toggleKey)
+
+        val hint = !config.disableHint && !config.alwaysEnabled && keyName != "NONE"
+        if (active || config.alwaysEnabled) {
+            event.toolTip = buildList {
+                add(event.toolTip.first())
+                if (hint) {
+                    add("§7Focus Mode from SkyHanni active!")
+                    add("Press $keyName to disable!")
+                }
+            }.toMutableList()
+        } else {
+            if (hint) {
+                event.toolTip.add(1, "§7Press $keyName to enable Focus Mode from SkyHanni!")
+            }
+        }
     }
 
     @SubscribeEvent
     fun onLorenzTick(event: LorenzTickEvent) {
         if (!isEnabled()) return
+        if (config.alwaysEnabled) return
         if (!config.toggleKey.isKeyClicked()) return
-        toggle = !toggle
+        active = !active
     }
 
     fun isEnabled() = LorenzUtils.inSkyBlock && InventoryUtils.inContainer() && config.enabled
