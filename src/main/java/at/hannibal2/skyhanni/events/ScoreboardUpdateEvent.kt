4750db953c4bec0b3fb22b1f57633a7cf3f1dc71
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-10-27 10:25:30 +0100

    Improvement: ScoreboardUpdateEvent (#2765)
    Co-authored-by: Empa <itsempa@users.noreply.github.com>
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/events/ScoreboardUpdateEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/ScoreboardUpdateEvent.kt
@@ -1,3 +1,10 @@
 package at.hannibal2.skyhanni.events
 
-class ScoreboardUpdateEvent(val scoreboard: List<String>) : LorenzEvent()
+class ScoreboardUpdateEvent(
+    val old: List<String>,
+    val scoreboard: List<String>,
+) : LorenzEvent() {
+
+    val added by lazy { scoreboard - old.toSet() }
+    val removed by lazy { old - scoreboard.toSet() }
+}
