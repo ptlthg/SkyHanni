2f2d5ffc33321667382a19e65beea8fcf4866439
HiZe <super@hize.be>
2024-11-01 20:20:07 +0100

    Fix: Skill Overflow (#2808)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuSkillLevelJson.kt
+++ src/main/java/at/hannibal2/skyhanni/data/jsonobjects/repo/neu/NeuSkillLevelJson.kt
@@ -5,4 +5,5 @@ import com.google.gson.annotations.SerializedName
 
 data class NeuSkillLevelJson(
     @Expose @SerializedName("leveling_xp") val levelingXp: List<Int>,
+    @Expose @SerializedName("leveling_caps") val levelingCaps: Map<String, Int>,
 )
