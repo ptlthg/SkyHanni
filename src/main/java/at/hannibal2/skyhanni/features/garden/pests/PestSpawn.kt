7a1987dad239132bd7ec8fe219c8da0a605b1c39
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2024-11-19 18:52:30 -0500

    Fix: Pest Spawn Patterns (#2954)
    Co-authored-by: Cal <cwolfson58@gmail.com>

--- src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawn.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/pests/PestSpawn.kt
@@ -26,43 +26,28 @@ object PestSpawn {
     private val patternGroup = RepoPattern.group("garden.pests.spawn")
 
     /**
-     * REGEX-TEST: §6§lGROSS! §7A §6Pest §7has appeared in §aPlot §7- §b4§7!
+     * REGEX-TEST: §6§lGROSS! §7A §2Pest §7has appeared in §aPlot §7- §b4§7!
      */
     private val onePestPattern by patternGroup.pattern(
         "one",
-        "§6§l.*! §7A §6Pest §7has appeared in §aPlot §7- §b(?<plot>.*)§7!"
+        "§6§l.*! §7A §2Pest §7has appeared in §aPlot §7- §b(?<plot>.*)§7!",
     )
 
     /**
-     * REGEX-TEST: §6§lGROSS! §7A §6Pest §7has appeared in §aThe Barn§7!
-     */
-    private val onePestBarnPattern by patternGroup.pattern(
-        "onebarn",
-        "§6§l.*! §7A §6Pest §7has appeared in §a(?<plot>The Barn)§7!"
-    )
-
-    /**
-     * REGEX-TEST: §6§lEWW! §62 Pests §7have spawned in §aPlot §7- §b2§7!
+     * REGEX-TEST: §6§lEWW! §22 Pests §7have spawned in §aPlot §7- §b2§7!
+     * REGEX-TEST: §6§lYUCK! §23 Pests §7have spawned in §aPlot §7- §bR1§7!
      */
     private val multiplePestsSpawn by patternGroup.pattern(
         "multiple",
-        "§6§l.*! §6(?<amount>\\d) Pests §7have spawned in §aPlot §7- §b(?<plot>.*)§7!"
+        "§6§l.*! §2(?<amount>\\d) Pests §7have spawned in §aPlot §7- §b(?<plot>.*)§7!",
     )
 
     /**
-     * REGEX-TEST: §6§lEWW! §62 Pests §7have spawned in §aThe Barn§7!
-     */
-    private val multiplePestsBarnSpawn by patternGroup.pattern(
-        "multiplebarn",
-        "§6§l.*! §6(?<amount>\\d) Pests §7have spawned in §a(?<plot>The Barn)§7!"
-    )
-
-    /**
-     * REGEX-TEST: §6§lGROSS! §7While you were offline, §6Pests §7spawned in §aPlots §r§b12§r§7, §r§b9§r§7, §r§b5§r§7, §r§b11§r§7 and §r§b3§r§r§7!
+     * REGEX-TEST: §6§lGROSS! §7While you were offline, §2Pests §7spawned in §aPlots §r§b12§r§7, §r§b9§r§7, §r§b5§r§7, §r§b11§r§7 and §r§b3§r§r§7!
      */
     private val offlinePestsSpawn by patternGroup.pattern(
         "offline",
-        "§6§l.*! §7While you were offline, §6Pests §7spawned in §aPlots (?<plots>.*)!"
+        "§6§l.*! §7While you were offline, §2Pests §7spawned in §aPlots (?<plots>.*)!",
     )
     private var plotNames = mutableListOf<String>()
 
@@ -80,20 +65,7 @@ object PestSpawn {
             pestSpawn(1, plotNames, false)
             blocked = true
         }
-        onePestBarnPattern.matchMatcher(message) {
-            val plotName = group("plot")
-            plotNames.add(plotName)
-            pestSpawn(1, plotNames, false)
-            blocked = true
-        }
         multiplePestsSpawn.matchMatcher(message) {
-            val plotName = group("plot")
-            plotNames.add(plotName)
-            val amount = group("amount").toInt()
-            pestSpawn(amount, plotNames, false)
-            blocked = true
-        }
-        multiplePestsBarnSpawn.matchMatcher(message) {
             val plotName = group("plot")
             plotNames.add(plotName)
             val amount = group("amount").toInt()
