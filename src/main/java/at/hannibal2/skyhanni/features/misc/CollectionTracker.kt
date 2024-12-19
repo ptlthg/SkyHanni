72103bee9e9246c93ad9363c5b34bc9dadaf7648
martimavocado <39881008+martimavocado@users.noreply.github.com>
2024-12-19 22:22:31 +0000

    Fix: shtrackcollection not counting Youngite and Obsolite when tracking timite (#3082)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/features/misc/CollectionTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/misc/CollectionTracker.kt
@@ -41,6 +41,12 @@ object CollectionTracker {
     private var recentGain = 0
     private var lastGainTime = -1L
 
+    private val CACTUS = "CACTUS".toInternalName()
+    private val CACTUS_GREEN = "INK_SACK-2".toInternalName()
+    private val YOUNGITE = "YOUNGITE".toInternalName()
+    private val OBSOLITE = "OBSOLITE".toInternalName()
+    private val TIMITE = "TIMITE".toInternalName()
+
     fun command(args: Array<String>) {
         if (args.isEmpty()) {
             if (internalName == null) {
@@ -167,16 +173,17 @@ object CollectionTracker {
         )
     }
 
-    private fun countCurrentlyInInventory(): Int {
-        val cactus = "CACTUS".toInternalName()
-        val cactusGreen = "INK_SACK-2".toInternalName()
-        return InventoryUtils.countItemsInLowerInventory {
-            if (internalName == cactus && it.getInternalName() == cactusGreen) {
-                return@countItemsInLowerInventory true
-            }
-            it.getInternalName() == internalName
+    private fun countCurrentlyInInventory(): Int = InventoryUtils.countItemsInLowerInventory {
+        val name = it.getInternalName()
+        if (internalName == CACTUS && name == CACTUS_GREEN) {
+            return@countItemsInLowerInventory true
         }
+        if (internalName == TIMITE && (name == YOUNGITE || name == OBSOLITE)) {
+            return@countItemsInLowerInventory true
+        }
+        name == internalName
     }
+
 
     fun handleTabComplete(command: String): List<String>? {
         if (command != "shtrackcollection") return null
