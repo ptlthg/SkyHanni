0fe7bc903aa544a01f53103e33083c79fc5a411c
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-12-24 11:42:16 +0100

    added comments

--- src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
@@ -14,6 +14,7 @@ import net.minecraft.util.AxisAlignedBB
 import net.minecraft.util.BlockPos
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
+// TODO move into mining category and package
 @SkyHanniModule
 object NucleusBarriersBox {
     private val config get() = SkyHanniMod.feature.mining.crystalHighlighter
--- src/main/java/at/hannibal2/skyhanni/utils/CircularList.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/CircularList.kt
@@ -1,5 +1,13 @@
 package at.hannibal2.skyhanni.utils
 
+/**
+ * A generic, immutable circular list that cycles through its elements infinitely.
+ *
+ * @param T The type of elements in the circular list.
+ * @property items A list of elements to be accessed in a circular manner.
+ * @constructor Creates a CircularList with the given elements.
+ * @throws IllegalArgumentException if the list is empty.
+ */
 class CircularList<T>(private val items: List<T>) {
 
     constructor(vararg elements: T) : this(elements.asList())
