619d1ad050880a7327a8cfbbb2b98c7942c7398f
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-04 16:39:45 -0500

    Improvement: Nucleus Highlight Colors (#3062)
    * Done
    
    * Remove comments since that worked
    
    * formatting
    
    ---------
    
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>
    Co-authored-by: hannibal2 <24389977+hannibal002@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/mining/CrystalHighlighterConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/mining/CrystalHighlighterConfig.java
@@ -2,15 +2,15 @@ package at.hannibal2.skyhanni.config.features.mining;
 
 import at.hannibal2.skyhanni.config.FeatureToggle;
 import com.google.gson.annotations.Expose;
+import io.github.notenoughupdates.moulconfig.annotations.Accordion;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorSlider;
 import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 
 public class CrystalHighlighterConfig {
 
     @Expose
     @ConfigOption(
-        name = "Highlight Crystal Nucleus barriers",
+        name = "Highlight Nucleus Barriers",
         desc = "Draw visible bounding boxes around the Crystal Nucleus crystal barrier blocks."
     )
     @ConfigEditorBoolean
@@ -18,12 +18,9 @@ public class CrystalHighlighterConfig {
     public boolean enabled = true;
 
     @Expose
-    @ConfigOption(
-        name = "Highlight Opacity",
-        desc = "Set the opacity of the highlighted boxes.\n§70§8: §7Transparent\n§7100§8: §7Solid"
-    )
-    @ConfigEditorSlider(minValue = 0, maxValue = 100, minStep = 1)
-    public int opacity = 60;
+    @Accordion
+    @ConfigOption(name = "Highlight Colors", desc = "")
+    public CrystalHighlighterColorConfig colors = new CrystalHighlighterColorConfig();
 
     @Expose
     @ConfigOption(
--- src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
+++ src/main/java/at/hannibal2/skyhanni/features/event/hoppity/NucleusBarriersBox.kt
@@ -6,10 +6,11 @@ import at.hannibal2.skyhanni.data.IslandType
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
 import at.hannibal2.skyhanni.events.skyblock.GraphAreaChangeEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
 import at.hannibal2.skyhanni.utils.RenderUtils.drawFilledBoundingBoxNea
 import at.hannibal2.skyhanni.utils.RenderUtils.expandBlock
+import at.hannibal2.skyhanni.utils.SpecialColor.toSpecialColor
+import io.github.notenoughupdates.moulconfig.observer.Property
 import net.minecraft.util.AxisAlignedBB
 import net.minecraft.util.BlockPos
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -18,44 +19,48 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 @SkyHanniModule
 object NucleusBarriersBox {
     private val config get() = SkyHanniMod.feature.mining.crystalHighlighter
+    private val colorConfig get() = config.colors
 
     private var inNucleus = false
 
-    private enum class Crystal(val color: LorenzColor, val boundingBox: AxisAlignedBB) {
+    private enum class Crystal(
+        val boundingBox: AxisAlignedBB,
+        val configColorOption: Property<String>,
+    ) {
         AMBER(
-            LorenzColor.GOLD,
             AxisAlignedBB(
                 BlockPos(474.0, 124.0, 524.0),
                 BlockPos(485.0, 111.0, 535.0),
             ).expandBlock(),
+            colorConfig.amber,
         ),
         AMETHYST(
-            LorenzColor.DARK_PURPLE,
             AxisAlignedBB(
                 BlockPos(474.0, 124.0, 492.0),
                 BlockPos(485.0, 111.0, 503.0),
             ).expandBlock(),
+            colorConfig.amethyst,
         ),
         TOPAZ(
-            LorenzColor.YELLOW,
             AxisAlignedBB(
                 BlockPos(508.0, 124.0, 473.0),
                 BlockPos(519.0, 111.0, 484.0),
             ).expandBlock(),
+            colorConfig.topaz,
         ),
         JADE(
-            LorenzColor.GREEN,
             AxisAlignedBB(
                 BlockPos(542.0, 124.0, 492.0),
                 BlockPos(553.0, 111.0, 503.0),
             ).expandBlock(),
+            colorConfig.jade,
         ),
         SAPPHIRE(
-            LorenzColor.BLUE,
             AxisAlignedBB(
                 BlockPos(542.0, 124.0, 524.0),
                 BlockPos(553.0, 111.0, 535.0),
             ).expandBlock(),
+            colorConfig.sapphire,
         ),
     }
 
@@ -71,7 +76,7 @@ object NucleusBarriersBox {
         Crystal.entries.forEach { crystal ->
             event.drawFilledBoundingBoxNea(
                 crystal.boundingBox,
-                crystal.color.addOpacity(config.opacity),
+                crystal.configColorOption.get().toSpecialColor(),
                 renderRelativeToCamera = false,
             )
         }
