0cde464e68d41a98c529b339c61dca56e0428197
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-08 20:49:28 -0500

    Backend: Config to Kotlin #1 (#3172)
    Co-authored-by: CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/PowderMiningGemstoneFilterConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/PowderMiningGemstoneFilterConfig.kt
@@ -1,68 +1,58 @@
-package at.hannibal2.skyhanni.config.features.chat;
+package at.hannibal2.skyhanni.config.features.chat
 
-import com.google.gson.annotations.Expose;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown;
-import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
+import com.google.gson.annotations.Expose
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorBoolean
+import io.github.notenoughupdates.moulconfig.annotations.ConfigEditorDropdown
+import io.github.notenoughupdates.moulconfig.annotations.ConfigOption
 
-public class PowderMiningGemstoneFilterConfig {
-
+class PowderMiningGemstoneFilterConfig {
     @Expose
     @ConfigOption(name = "Stronger Tool Messages", desc = "Hide 'You need a stronger tool..' messages.")
     @ConfigEditorBoolean
-    public boolean strongerToolMessages = true;
+    var strongerToolMessages: Boolean = true
 
     @Expose
     @ConfigOption(name = "Ruby", desc = "Hide Ruby gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry rubyGemstones = GemstoneFilterEntry.FINE_UP;
+    var rubyGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Sapphire", desc = "Hide Sapphire gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry sapphireGemstones = GemstoneFilterEntry.FINE_UP;
+    var sapphireGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Amber", desc = "Hide Amber gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry amberGemstones = GemstoneFilterEntry.FINE_UP;
+    var amberGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Amethyst", desc = "Hide Amethyst gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry amethystGemstones = GemstoneFilterEntry.FINE_UP;
+    var amethystGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Jade", desc = "Hide Jade gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry jadeGemstones = GemstoneFilterEntry.FINE_UP;
+    var jadeGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Topaz", desc = "Hide Topaz gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry topazGemstones = GemstoneFilterEntry.FINE_UP;
+    var topazGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
     @Expose
     @ConfigOption(name = "Jasper", desc = "Hide Jasper gemstones under a certain quality.")
     @ConfigEditorDropdown
-    public GemstoneFilterEntry jasperGemstones = GemstoneFilterEntry.FINE_UP;
+    var jasperGemstones: GemstoneFilterEntry = GemstoneFilterEntry.FINE_UP
 
-    public enum GemstoneFilterEntry {
+    enum class GemstoneFilterEntry(private val displayName: String) {
         SHOW_ALL("Show All"),
         HIDE_ALL("Hide all"),
         FLAWED_UP("Show §aFlawed §7or higher"),
         FINE_UP("Show §9Fine §7or higher"),
         FLAWLESS_ONLY("Show §5Flawless §7only");
 
-        private final String str;
-
-        GemstoneFilterEntry(String str) {
-            this.str = str;
-        }
-
-        @Override
-        public String toString() {
-            return str;
-        }
+        override fun toString() = displayName
     }
 }
