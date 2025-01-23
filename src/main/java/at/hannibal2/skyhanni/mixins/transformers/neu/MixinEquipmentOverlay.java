0c3d1fa066ff7aa35a2ee24109aac1e16ebf0a0a
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2025-01-23 21:25:11 +0100

    fixed sh crash without neu (ty empa)

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/neu/MixinEquipmentOverlay.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/neu/MixinEquipmentOverlay.java
@@ -4,12 +4,14 @@ import at.hannibal2.skyhanni.features.misc.items.EstimatedItemValue;
 import io.github.moulberry.notenoughupdates.overlays.EquipmentOverlay;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
+@Pseudo
 @Mixin(value = EquipmentOverlay.class, remap = false)
 public class MixinEquipmentOverlay {
 
--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/neu/MixinStorageOverlay.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/neu/MixinStorageOverlay.java
@@ -3,10 +3,12 @@ package at.hannibal2.skyhanni.mixins.transformers.neu;
 import at.hannibal2.skyhanni.features.misc.items.EstimatedItemValue;
 import io.github.moulberry.notenoughupdates.miscgui.StorageOverlay;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(value = StorageOverlay.class, remap = false)
 public class MixinStorageOverlay {
 
