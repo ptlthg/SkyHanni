4ee83494e9779f25a5352cb81582fd50ac868854
David Cole <40234707+DavidArthurCole@users.noreply.github.com>
2025-01-24 12:59:08 -0500

    Backend: Storage Needs @Expose (#3292)
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

---
+++ detekt/src/main/kotlin/potentialbugs/PotentialBugsProvider.kt
@@ -0,0 +1,19 @@
+package at.hannibal2.skyhanni.detektrules.potentialbugs
+
+import com.google.auto.service.AutoService
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+@AutoService(RuleSetProvider::class)
+class PotentialBugsProvider : RuleSetProvider {
+    override val ruleSetId: String
+        get() = "potential-bugs"
+
+    override fun instance(config: Config): RuleSet {
+        return RuleSet(
+            ruleSetId,
+            listOf(StorageNeedsExpose(config)),
+        )
+    }
+}
