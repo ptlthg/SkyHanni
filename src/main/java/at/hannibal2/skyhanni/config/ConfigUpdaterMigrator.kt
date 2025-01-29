b2031c3ec2d8bc56571bda90181b0485493cfaa9
Empa <42304516+ItsEmpa@users.noreply.github.com>
2025-01-29 19:40:34 +0100

    Backend: Kotlin Position (#2855)
    Co-authored-by: Empa <itsempa@users.noreply.github.com>
    Co-authored-by: CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
    Co-authored-by: Cal <cwolfson58@gmail.com>
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/ConfigUpdaterMigrator.kt
+++ src/main/java/at/hannibal2/skyhanni/config/ConfigUpdaterMigrator.kt
@@ -12,7 +12,7 @@ import com.google.gson.JsonPrimitive
 object ConfigUpdaterMigrator {
 
     val logger = LorenzLogger("ConfigMigration")
-    const val CONFIG_VERSION = 71
+    const val CONFIG_VERSION = 72
     fun JsonElement.at(chain: List<String>, init: Boolean): JsonElement? {
         if (chain.isEmpty()) return this
         if (this !is JsonObject) return null
@@ -69,7 +69,7 @@ object ConfigUpdaterMigrator {
                     move(
                         since,
                         "$realPrefix.${oldPath.substringAfter('.')}",
-                        "$realPrefix.${newPath.substringAfter('.')}", transform
+                        "$realPrefix.${newPath.substringAfter('.')}", transform,
                     )
                     return
                 }
@@ -83,7 +83,7 @@ object ConfigUpdaterMigrator {
             if (newParentElement !is JsonObject) {
                 logger.log(
                     "Catastrophic: element at path $old could not be relocated to $new, " +
-                        "since another element already inhabits that path"
+                        "since another element already inhabits that path",
                 )
                 return
             }
@@ -140,7 +140,7 @@ object ConfigUpdaterMigrator {
                 JsonObject().also {
                     it.add("lastVersion", JsonPrimitive(i + 1))
                 },
-                i, 0, dynamicPrefix
+                i, 0, dynamicPrefix,
             ).also { it.post() }
             logger.log("Transformations scheduled: ${migration.new}")
             val mergesPerformed = merge(migration.old, migration.new)
