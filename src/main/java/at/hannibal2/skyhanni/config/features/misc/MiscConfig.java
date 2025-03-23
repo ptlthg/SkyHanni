b9067348c8f6aca0673426c45a138338d9327de1
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-03-23 13:31:54 +0100

    Improvement: Even better fairy soul logic (#3427)
    * Added NEU Soul Path Find
    
    * typos
    
    * started playing with better pathfind logic
    
    * better pathfinding logic already works semi well
    
    * fixed hiding some souls
    
    * added options for better
    
    * fix merge conflicts
    
    * fixed inconsistency and added foundButNotClickedSoul logic
    
    * more fixes
    
    * make detekt happy
    
    * extract getRoute
    
    * extracted into NavigationUtils
    
    * added test option for variable effort in optimising the path, and add optimizeCriticalSegments
    
    * workaround solution for wrong neu data
    
    * use fast path finder
    
    * added todo
    
    * stored in storage and use repo patterns
    
    * use even better navigation logic
    
    * caching render state, showing edges all the time
    
    * fixes
    
    * cleanup
    
    * formatting
    
    * formatting
    
    * show faster solution  suggestion
    
    * added suggestions
    
    ---------
    
    Co-authored-by: hannibal2 <24389977+hannibal00212@users.noreply.github.com>
    Co-authored-by: CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>

--- src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.java
+++ src/main/java/at/hannibal2/skyhanni/config/features/misc/MiscConfig.java
@@ -378,10 +378,15 @@ public class MiscConfig {
     public boolean maintainGameVolume = false;
 
     @Expose
-    @ConfigOption(name = "NEU Soul Path Find", desc = "When showing §e/neusouls on§7, show a pathfind to the Fairy Souls missing and a percentage of souls done in chat.")
+    @ConfigOption(name = "NEU Soul Path Find", desc = "When showing §e/neusouls on§7, show a pathfind to the faily souls missing and a percentage of souls done in chat.")
     @ConfigEditorBoolean
     @FeatureToggle
     public boolean neuSoulsPathFind = true;
+
+    @Expose
+    @ConfigOption(name = "Fast Fairy Souls", desc = "Uses a fast pathfinder route to get to all Fairy Souls on the current island. §eDoes not require NEU. ")
+    @ConfigEditorBoolean
+    public boolean fastFairySouls = false;
 
     @Expose
     @ConfigOption(name = "GFS Piggy Bank", desc = "When your Piggy Bank breaks, send a chat warning to get enchanted pork from sacks.")
@@ -393,6 +398,7 @@ public class MiscConfig {
     @ConfigOption(name = "SkyHanni User Luck", desc = "Shows SkyHanni User Luck in the SkyBlock Stats.")
     @ConfigEditorBoolean
     @FeatureToggle
+    // TODO rename to userLuck
     public boolean userluckEnabled = true;
 
     @Expose
