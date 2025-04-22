9b94b1f30a04675dce3c8ccc8b1964ed178cdbca
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-04-22 16:39:54 +0200

    Fix: Sea Creature Highlight (#3830)

--- src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinEntity.java
+++ src/main/java/at/hannibal2/skyhanni/mixins/transformers/MixinEntity.java
@@ -1,15 +1,18 @@
 package at.hannibal2.skyhanni.mixins.transformers;
 
 import at.hannibal2.skyhanni.data.EntityData;
+import at.hannibal2.skyhanni.features.fishing.SeaCreatureFeatures;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
-
     @ModifyVariable(
         method = "getDisplayName",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ChatStyle;setInsertion(Ljava/lang/String;)Lnet/minecraft/util/ChatStyle;", shift = At.Shift.AFTER)
