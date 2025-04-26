cf03134a5d8d53da8f08160a0892fb17f4170e30
hannibal2 <24389977+hannibal002@users.noreply.github.com>
2025-04-27 00:17:08 +0200

    detekt doesn't like todo comments after annotations

--- src/main/java/at/hannibal2/skyhanni/config/features/rift/RiftConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/rift/RiftConfig.kt
@@ -36,8 +36,7 @@ class RiftConfig {
     @Expose
     @ConfigOption(name = "Motes Orbs", desc = "")
     @Accordion
-    // TODO move into MotesConfig
-    var motesOrbs: MotesOrbsConfig = MotesOrbsConfig()
+    var motesOrbs: MotesOrbsConfig = MotesOrbsConfig() // TODO move into MotesConfig
 
     @Expose
     @ConfigOption(name = "Punchcard Artifact", desc = "")
@@ -60,7 +59,7 @@ class RiftConfig {
     @ConfigOption(
         name = "Temporal Pillar Dodge",
         desc = "Avoid pathfinding solutions through or near the big enderman " +
-            "that throws you back to the end credits screen and steals so much time."
+            "that throws you back to the end credits screen and steals so much time.",
     )
     @ConfigEditorBoolean
     @FeatureToggle
